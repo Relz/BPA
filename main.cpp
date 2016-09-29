c61569b15b798c20d19ae8c4cdaa00228fba7083
Relz <relz0071@gmail.com>
2016-09-29 17:27:11 +0300

    Удалены лишние инклуды; Добавлена функция проверки введенной высоты: должна быть положительным числом и не превышать максимальное значение int; Выведена константа ускорения свободного падения; Переименованы переменные; Добавлена русская локализация;

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <cmath>
+#include <math.h>
+#include <limits>
+
+static const float G = 9.8;
+static const int MAX_HEIGHT = std::numeric_limits<int>::max();
 
 /*
  * Программа получает на вход высоту прыжка.
@@ -21,48 +23,54 @@
  *      b) вывод времени достижения начальной высоты прыжка (конец прыжка)
  */
 
-// TODO: Rename variables and split to several functions,
-// TODO: fix negative height values, fix heigh values higher than max height.
-
+bool isValidHeight(float height)
+{
+    return (height >= 0 && height <= MAX_HEIGHT);
+}
 
 int main(int, char *[])
 {
-	const float g = 9.8f;
-	float T;
-	float Vn;
-	int S;
-	int M;
-	printf("S: ");
-	if (0 == scanf("%d", &S))
-	{
-		printf("\n" "expected floating-point number" "\n");
-		exit(1);
-	}
-	// T - time point when height is at maximum.
-	// t - current time point
-	// v(t) == v0 - g * t
-	// v0 = g * T
-	// s(t) == v0 * t - 0.5 * g * t * t
-	T = sqrt(S * 2 / g);
-	printf("T=%f\n", T);
-	bool flag = false;
-	for (float t = 0; t < T * 2; t += 0.1f)
-	{
-		if (t > T && !flag)
-		{
-			flag = true;
-			float V0 = g * T;
-			float s = V0 * T - 0.5 * g * T * T;
-			printf("t=%f, s=%f\n", T, s);
-		}
-		float V0 = g * T;
-		float s = V0 * t - 0.5 * g * t * t;
-		printf("t=%f, s=%f\n", t, s);
-	}
+    setlocale(LC_ALL, "rus");
+    int maxHeight;
 
-	float V0 = g * T;
-	float s = V0 * (T*2) - 0.5 * g * (T * 2) * (T * 2);
-	printf("t=%f, s=%f\n", T * 2, s);
+    printf("Максимальная высота прыжка: ");
+    if (0 == scanf("%d", &maxHeight))
+    {
+        printf("\nТребуется ввести целое число\n");
+        return 1;
+    }
 
-	return 0;
+    if (!isValidHeight(maxHeight))
+    {
+        printf("\nМаксимальная высота прыжка должна быть положительным числом и менее %d\n", MAX_HEIGHT);
+        return 1;
+    }
+
+    float timeAtMaxHeight = sqrt(maxHeight * 2 / G);
+    printf("Время, при которой достигается максимальная высота = %f\n", timeAtMaxHeight);
+
+    float initialSpeed, currHeight;
+    bool flag = false;
+
+    for (float currTime = 0; currTime < timeAtMaxHeight * 2; currTime += 0.1f)
+    {
+        if (currTime > timeAtMaxHeight && !flag)
+        {
+            flag = true;
+            initialSpeed = G * timeAtMaxHeight;
+            currHeight = initialSpeed * timeAtMaxHeight - 0.5f * G * pow(timeAtMaxHeight, 2);
+            printf("Время, при которой достигается максимальная высота = %f, текущая высота = %f\n", timeAtMaxHeight,
+                   currHeight);
+        }
+        initialSpeed = G * timeAtMaxHeight;
+        currHeight = initialSpeed * currTime - 0.5f * G * pow(currTime, 2);
+        printf("Текущее время = %f, текущая высота = %f\n", currTime, currHeight);
+    }
+
+    initialSpeed = G * timeAtMaxHeight;
+    currHeight = initialSpeed * (timeAtMaxHeight * 2) - 0.5f * G * pow(timeAtMaxHeight * 2, 2);
+    printf("Время, при которой достигается максимальная высота = %f, текущая высота = %f\n", timeAtMaxHeight * 2,
+           currHeight);
+
+    return 0;
 }
