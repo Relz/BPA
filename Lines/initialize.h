14c4b439bd988ae4ed54b29bfa844acb78cb2c2e
Relz <relz0071@gmail.com>
2016-11-05 21:41:31 +0300

    Удалены лишние объявления функций в initialize

--- Lines/initialize.h
+++ Lines/initialize.h
@@ -3,9 +3,6 @@
 
 #include "stdafx.h"
 
-void initGameField(GameField &gameField, Vector2f &windowCenter);
-bool initGameTopBar(GameView &gameView);
-bool initGameOverView(GameView &gameView);
 bool initGameView(RenderWindow &window, GameView &gameView);
 void initFreePositionsOnField(vector<PositionOnField> &freePositionsOnField);
 
