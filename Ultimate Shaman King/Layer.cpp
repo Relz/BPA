a2094cddb412b8c48fc6ffa0046ab08c079836de
Relz <relz0071@gmail.com>
2017-01-09 15:32:02 +0300

    Изменено название проекта: удалены пробелы, так как PVS Studio не смог связаться с проектом, в пути которого содержатся пробелы. Реализована загрузка tmx с обработкой Тайлов, Объектов окружения и Блоков коллизии. Добавлен игрок, управление им, анимация состояния покоя, движения и прыжка. Добавлена Физика: Коллизия и Гравитация.

--- Ultimate Shaman King/Layer.cpp
+++
@@ -1,2 +0,0 @@
-#include "stdafx.h"
-#include "Layer.h"
--- Ultimate Shaman King/handleEvents.h
+++
@@ -1,8 +0,0 @@
-#ifndef ULTIMATE_SHAMAN_KING_HANDLEEVENTS_H
-#define ULTIMATE_SHAMAN_KING_HANDLEEVENTS_H
-
-#include "Yoh.h"
-
-void handleEvents(sf::RenderWindow &window, b2Body **Body);
-
-#endif //ULTIMATE_SHAMAN_KING_HANDLEEVENTS_H
